f039a9eb6574cabdabc340098c744125a0fe7156
Krutarth Patel <kishanpatel9702@gmail.com>
2024-05-04 16:58:50 +0530

    leetcode 35

---
+++ l35.java
@@ -0,0 +1,37 @@
+public class l35 {
+  
+    public static int searchInsert(int[] nums, int target) {
+
+      int start=0;
+      int end=nums.length-1;
+
+      while(start<=end){
+
+        int mid=(start+end)/2;
+        if(nums[mid]==target){
+          return mid;
+        }
+        else if(nums[mid]>target){
+          end=mid-1;
+        }
+        else{
+          start=mid+1;
+        }
+
+      }
+      return start;
+
+   
+        
+    }
+public static void main(String[] args) {
+
+  int[] arr={1,3,5,6};
+  int t=7;
+
+  System.out.println(searchInsert(arr,t));
+  
+  
+}
+  
+}
